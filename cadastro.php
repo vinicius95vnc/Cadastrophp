2187cebd3f37cba533fd82834c2b013c43655d43
Vinicius Gomes <78374367+vinicius95vnc@users.noreply.github.com>
2021-06-10 14:17:15 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,15 @@
+<?php
+$nome = $_POST['nome do produto'];
+$serial = $_POST['serial do produto'];
+$descricão = $_POST['Descrição do produto'];
+$TipoProduto = $_POST['tipo do produto'];
+$categoria  = $_POST['categoria do produto'];
+$sqli = mysqli_connect('localhost' , '' , 'tehx123' , 'Cadastro') or die ('Erro ao conectar ao Banco de Dados');
+$sql = "INSERT INTO Cadastro VALUES";
+$sql .="('$nome' , '$serial' , '$descricao' , '$TipoProduto' , '$categoria')";
+$mysqli_query($sqli, $sql) or die ("Erro ao tentar cadastrar registo");
+$mysqli_close($sqli);
+echo "Produto cadastrado com sucesso!";
+echo "<a href='sistemacadastro.html'>Clique aqui para realizar um novo cadastro</a><br>";
+echo "<a href='cadastro.php'>Clique Aqui para realizar uma consulta</a><br>";
+?>
